3b463ec1dfbcd4be9088381352fc1d7fdf602372
Afrie Irham <afrieirham.work@gmail.com>
2022-12-14 17:30:35 +0800

    test cancel in progress

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Firebase Next.js!
+          Hello Firebase Next.js!
         </h1>
 
         <p className={styles.description}>
